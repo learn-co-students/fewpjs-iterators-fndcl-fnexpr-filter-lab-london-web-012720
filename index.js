4046b2c16b498f6e19ea0ca9e756d41e41f2f152
Bryn <thenazboz@gmail.com>
2020-03-18 10:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, match) {
+    return arr.filter( person => 
+        person.toLowerCase() === match.toLowerCase()
+    )
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
